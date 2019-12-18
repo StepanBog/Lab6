723242f579caa67cf0ae1bd67018c8c5d81780aa
stepanbog <step300699@yandex.ru>
2019-12-18 10:20:01 +0300

    Scripted auto-commit on change (2019-12-18 10:20:01) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -1,4 +1,7 @@
 import akka.actor.ActorRef;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 public class Server {
@@ -7,5 +10,10 @@ public class Server {
     public Server(ZooKeeper zoo, ActorRef storage){
         this.zoo = zoo;
         this.storage = storage;
+    }
+
+    public void createServer(String host, int port) throws KeeperException, InterruptedException {
+        zoo.create("/servers/",(host + ":" + port).getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
     }
 }
