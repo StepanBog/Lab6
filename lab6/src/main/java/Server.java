18732e5bd11509f211a8e0d7ef524b843543ee29
stepanbog <step300699@yandex.ru>
2019-12-18 10:13:35 +0300

    Scripted auto-commit on change (2019-12-18 10:13:35) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -1,2 +1,8 @@
 public class Server {
+    private int serverNumb;
+    private int port;
+    public Server(int serverNumb,int port){
+        this.serverNumb = serverNumb;
+        this.port = port;
+    }
 }
