b92ea97f20adb8cd43719a41f8b4bbfce67665bf
stepanbog <step300699@yandex.ru>
2019-12-18 22:16:50 +0300

    Scripted auto-commit on change (2019-12-18 22:16:50) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -36,7 +36,12 @@ public class Server {
     }
 
     public void  saveServers(List<String> servers){
-      
+        for (int i= 0;
+            i < servers.size(); i++ ) {
+            System.out.print(servers.get(i) + " ");
+
+        }
+        System.out.println();
         storage.tell(new PutSeverList(servers),ActorRef.noSender());
     }
 }
