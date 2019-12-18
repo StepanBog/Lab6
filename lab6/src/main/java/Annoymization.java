a839f31c0b2fa9ee577104878834e5d3c4c618d4
stepanbog <step300699@yandex.ru>
2019-12-18 10:34:08 +0300

    Scripted auto-commit on change (2019-12-18 10:34:08) by gitwatch.sh

--- lab6/src/main/java/Annoymization.java
+++ lab6/src/main/java/Annoymization.java
@@ -6,9 +6,11 @@ import org.asynchttpclient.AsyncHttpClient;
 
 public class Annoymization {
     private AsyncHttpClient asyncHttpClient;
+    private ActorRef storage;
 
     public Annoymization(AsyncHttpClient asyncHttpClient, ActorRef storage, ActorMaterializer materializer, ZooKeeper zoo, Http http) {
 
         this.asyncHttpClient = asyncHttpClient;
+        this.storage = storage;
     }
 }
