76d4c6af50da39708cdeea579b477f53a714cbfd
stepanbog <step300699@yandex.ru>
2019-12-18 09:44:52 +0300

    Scripted auto-commit on change (2019-12-18 09:44:52) by gitwatch.sh

--- lab6/src/main/java/Lab6.java
+++ lab6/src/main/java/Lab6.java
@@ -1,2 +1,47 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import scala.concurrent.Future;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
+
+
 public class Lab6 {
+    private ActorRef routerActor;
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("lab4");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
+    }
+
 }
+
