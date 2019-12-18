18c0d00be9b973772dab917b2c5a8645ecf2c412
stepanbog <step300699@yandex.ru>
2019-12-18 15:14:29 +0300

    Scripted auto-commit on change (2019-12-18 15:14:29) by gitwatch.sh

--- lab6/src/main/java/Annoymization.java
+++ lab6/src/main/java/Annoymization.java
@@ -70,7 +70,8 @@ public class Annoymization {
     private Request createRequest(String server,String url, int count){
         return asyncHttpClient.prepareGet(server)
                 .addQueryParam("url",url)
-                .addQueryParam("count",count)
+                .addQueryParam("count",Integer.toString(count))
+                .build();
     }
 
     private CompletionStage<Response> fetch(Request request) {
