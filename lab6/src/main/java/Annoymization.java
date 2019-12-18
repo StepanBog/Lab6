e676757b81ac08d131bfb7daa2087de36f69c70d
stepanbog <step300699@yandex.ru>
2019-12-18 13:32:04 +0300

    Scripted auto-commit on change (2019-12-18 13:32:04) by gitwatch.sh

--- lab6/src/main/java/Annoymization.java
+++ lab6/src/main/java/Annoymization.java
@@ -47,7 +47,7 @@ public class Annoymization {
             responce = fetch(asyncHttpClient.prepareGet(url).build());
         else
             responce;
-        return CompletionStage<Response> 
+        return Compileresponce
     }
 
     private CompletionStage<Response> fetch(Request request) {
