ed2b8faa60c25a71d3a94c97cfa9d44701251129
stepanbog <step300699@yandex.ru>
2019-12-18 15:18:46 +0300

    Scripted auto-commit on change (2019-12-18 15:18:45) by gitwatch.sh

--- lab6/src/main/java/Annoymization.java
+++ lab6/src/main/java/Annoymization.java
@@ -60,7 +60,7 @@ public class Annoymization {
         return FutureConverters.toJava(Patterns.ask(storage,new GetRandomServer(),5000))
                 .thenApply(s-> (ServerMsg)s)
                 .thenApply(ser -> ser.getRandServer())
-                .thenCompose(server -> fetch(createRequest(getServerUrl(server),url,count)))
+                .thenCompose(server -> fetch(createRequest(getServerUrl(server),url,count))
                 .handle()
     }
 
